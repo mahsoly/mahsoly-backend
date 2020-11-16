1c44a2feadfd0792a6feaf63f916404b5e20d69c
Ahmed Ehab <ahmed.ehab5010@gmail.com>
2020-11-16 10:05:01 +0200

    Add project reactor, blockhound, spring security and spring jpa

--- src/main/java/com/mahsoly/app/MahsolyBackendApplication.java
+++ src/main/java/com/mahsoly/app/MahsolyBackendApplication.java
@@ -3,10 +3,15 @@ package com.mahsoly.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.blockhound.BlockHound;
+import reactor.tools.agent.ReactorDebugAgent;
+
 @SpringBootApplication
 public class MahsolyBackendApplication {
 
 	public static void main(String[] args) {
+		ReactorDebugAgent.init();
+		BlockHound.install();
 		SpringApplication.run(MahsolyBackendApplication.class, args);
 	}
 
